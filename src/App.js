0cc1f3419c25ddf1ff141b129906e614d8139e2c
daddy <sharmakavyaop702@gmail.com>
2023-08-07 22:15:51 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
+import {useEffect} from "react"
 import './App.css';
+import Card from "./components/card"
+import {useGlobalState} from './GlobalContext'
+import {useNavigate} from 'react-router-dom'
 
 function App() {
+  const navigate = useNavigate()
+  const{globalState, setGlobalState} = useGlobalState()
+  
+  async function getdata(){
+    const res = await fetch("https://api.tvmaze.com/search/shows?q=all", {method:"GET"})
+    if(res.ok){
+      return res.json()
+    }else{
+      const err = await res.json()
+      const em = err.error;
+      throw Error(em)
+    }
+  
+  }
+  useEffect(() => {
+    async function fetchLoggedinuser() {
+      try {
+        const res = await getdata();
+        setGlobalState(prevState => ({
+          ...prevState,
+          body: res,
+        }));
+        let arr = []
+        localStorage.setItem('bookings', JSON.stringify(arr))
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchLoggedinuser();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <h1 className="h-app">Show Explorer</h1>
+      <div className="cont">
+        {
+          globalState.body &&
+          globalState.body.map((m, index)=>{
+            
+            return(
+              <Card name = {m.show.name}  language = {m.show.language} score = {m.score} key = {index} genre = {m.show.genres} link = {m.show.image? m.show.image.medium : "https://www.allianceplast.com/wp-content/uploads/no-image-1024x1024.png"}clickfunc={()=> {
+                setGlobalState(prevState => ({
+                  ...prevState,
+                  key: index+1
+                }));
+                navigate("/components")
+
+              }}
+              
+              />
+            )
+          })
+        }
+      </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import D from "./components/details"
+import reportWebVitals from "./reportWebVitals";
+import { GlobalStateProvider } from "./GlobalContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalStateProvider>
+     <BrowserRouter>
+        <Routes>
+          
+          <Route path="/" Component={App}/> 
+          <Route path="/components" Component={D}/>
+        </Routes>
+      </BrowserRouter>
+      
+    </GlobalStateProvider>
   </React.StrictMode>
 );
 
